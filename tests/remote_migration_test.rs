8c9621a071ebf9f6a9cb7fb334351abd6cb69c48
Prashant <prashantco111@gmail.com>
2025-04-24 10:39:26 +0530

    Completed remote migration testing

--- tests/remote_migration_test.rs
+++ tests/remote_migration_test.rs
@@ -68,54 +68,199 @@ mod migration_tests {
     }
 
     mod migration {
-        use libsql_migration::errors::LibsqlRemoteMigratorError;
-
         use super::super::*;
         use crate::migration_tests::setup_test_db;
 
         #[tokio::test]
-        async fn establish_connection() -> Result<(), Box<dyn std::error::Error>> {
+        async fn check_for_valid_execution() -> Result<(), Box<dyn std::error::Error>> {
             let (conn, _temp_dir) = setup_test_db().await?;
 
             migrate(&conn, String::from("https://raw.githubusercontent.com/prashant1k99/libsql_migration/refs/heads/main/tests/remote-sql/v1.json")).await?;
 
             // Check for migrations_table
             let mut stmt = conn
-                .prepare("SELECT name FROM sqlite_master WHERE type='table' AND name = ?;")
+                .prepare("SELECT name FROM sqlite_master WHERE type='table';")
                 .await?;
 
-            let mut rows = stmt.query(["libsql_migrations"]).await?;
+            let mut rows = stmt.query(()).await?;
 
-            assert!(rows.next().await?.is_some(), "Migrations table not found");
+            let mut all_table_records: Vec<libsql::Value> = vec![];
+
+            while let Some(row) = rows.next().await? {
+                all_table_records.push(row.get_value(0)?);
+            }
+
+            assert!(
+                all_table_records.contains(&libsql::Value::Text(String::from("test1"))),
+                "Failed to create test1 migrations"
+            );
+
+            assert!(
+                all_table_records.contains(&libsql::Value::Text(String::from("test2"))),
+                "Failed to create test2 migrations"
+            );
+
+            let mut rows = conn
+                .query("PRAGMA table_info('test1');", libsql::params![])
+                .await?;
+
+            let mut column_info: std::collections::HashMap<String, String> =
+                std::collections::HashMap::new();
+
+            while let Some(row) = rows.next().await? {
+                let name = row.get::<String>(1)?;
+                let type_name = row.get::<String>(2)?.to_uppercase();
+                column_info.insert(name, type_name);
+            }
+
+            assert_eq!(
+                column_info.get_key_value("id"),
+                Some((&String::from("id"), &String::from("INTEGER")))
+            );
+
+            assert_eq!(
+                column_info.get_key_value("status"),
+                Some((&String::from("status"), &String::from("BOOLEAN")))
+            );
+
+            let mut rows = conn
+                .query("PRAGMA table_info('test2');", libsql::params![])
+                .await?;
+
+            let mut column_info: std::collections::HashMap<String, String> =
+                std::collections::HashMap::new();
+
+            while let Some(row) = rows.next().await? {
+                let name = row.get::<String>(1)?;
+                let type_name = row.get::<String>(2)?;
+                column_info.insert(name, type_name);
+            }
+
+            assert_eq!(
+                column_info.get_key_value("id"),
+                Some((&String::from("id"), &String::from("INTEGER")))
+            );
+
+            assert_eq!(
+                column_info.get_key_value("status"),
+                Some((&String::from("status"), &String::from("BOOLEAN")))
+            );
 
             Ok(())
         }
 
         #[tokio::test]
-        async fn invalid_url() -> Result<(), Box<dyn std::error::Error>> {
+        async fn should_not_execute_new_script_with_same_id()
+        -> Result<(), Box<dyn std::error::Error>> {
             let (conn, _temp_dir) = setup_test_db().await?;
 
-            assert!(matches!(
-                migrate(
-                    &conn,
-                    String::from("https://jsonplaceholder.typicode.com/todos/1"),
-                )
-                .await,
-                Err(LibsqlRemoteMigratorError::ReqwestError(_))
-            ));
+            migrate(&conn, String::from("https://raw.githubusercontent.com/prashant1k99/libsql_migration/refs/heads/main/tests/remote-sql/v1.json")).await?;
+            migrate(&conn, String::from("https://raw.githubusercontent.com/prashant1k99/libsql_migration/refs/heads/main/tests/remote-sql/v3.json")).await?;
+
+            let mut rows = conn
+                .query("PRAGMA table_info('test1');", libsql::params![])
+                .await?;
+
+            let mut column_info: std::collections::HashMap<String, String> =
+                std::collections::HashMap::new();
+
+            while let Some(row) = rows.next().await? {
+                let name = row.get::<String>(1)?;
+                let type_name = row.get::<String>(2)?.to_uppercase();
+                column_info.insert(name, type_name);
+            }
+
+            assert_eq!(column_info.get_key_value("Email"), None);
 
             Ok(())
         }
 
         #[tokio::test]
-        async fn empty_url() -> Result<(), Box<dyn std::error::Error>> {
+        async fn should_execute_correctly_for_multiple_urls()
+        -> Result<(), Box<dyn std::error::Error>> {
             let (conn, _temp_dir) = setup_test_db().await?;
 
-            assert!(matches!(
-                migrate(&conn, String::from(""),).await,
-                Err(LibsqlRemoteMigratorError::MigrationUrlNotValid(_))
-            ));
+            migrate(&conn, String::from("https://raw.githubusercontent.com/prashant1k99/libsql_migration/refs/heads/main/tests/remote-sql/v1.json")).await?;
+            migrate(&conn, String::from("https://raw.githubusercontent.com/prashant1k99/libsql_migration/refs/heads/main/tests/remote-sql/v2.json")).await?;
 
+            // Check for migrations_table
+            let mut stmt = conn
+                .prepare("SELECT name FROM sqlite_master WHERE type='table';")
+                .await?;
+
+            let mut rows = stmt.query(()).await?;
+
+            let mut all_table_records: Vec<libsql::Value> = vec![];
+
+            while let Some(row) = rows.next().await? {
+                all_table_records.push(row.get_value(0)?);
+            }
+
+            assert!(
+                all_table_records.contains(&libsql::Value::Text(String::from("test1"))),
+                "Failed to create test1 migrations"
+            );
+
+            assert!(
+                all_table_records.contains(&libsql::Value::Text(String::from("test2"))),
+                "Failed to create test2 migrations"
+            );
+
+            let mut rows = conn
+                .query("PRAGMA table_info('test1');", libsql::params![])
+                .await?;
+
+            let mut column_info: std::collections::HashMap<String, String> =
+                std::collections::HashMap::new();
+
+            while let Some(row) = rows.next().await? {
+                let name = row.get::<String>(1)?;
+                let type_name = row.get::<String>(2)?.to_uppercase();
+                column_info.insert(name, type_name);
+            }
+
+            assert_eq!(
+                column_info.get_key_value("id"),
+                Some((&String::from("id"), &String::from("INTEGER")))
+            );
+
+            assert_eq!(
+                column_info.get_key_value("status"),
+                Some((&String::from("status"), &String::from("BOOLEAN")))
+            );
+
+            assert_eq!(
+                column_info.get_key_value("Email"),
+                Some((&String::from("Email"), &String::from("TEXT")))
+            );
+
+            let mut rows = conn
+                .query("PRAGMA table_info('test2');", libsql::params![])
+                .await?;
+
+            let mut column_info: std::collections::HashMap<String, String> =
+                std::collections::HashMap::new();
+
+            while let Some(row) = rows.next().await? {
+                let name = row.get::<String>(1)?;
+                let type_name = row.get::<String>(2)?;
+                column_info.insert(name, type_name);
+            }
+
+            assert_eq!(
+                column_info.get_key_value("id"),
+                Some((&String::from("id"), &String::from("INTEGER")))
+            );
+
+            assert_eq!(
+                column_info.get_key_value("status"),
+                Some((&String::from("status"), &String::from("BOOLEAN")))
+            );
+
+            assert_eq!(
+                column_info.get_key_value("Email"),
+                Some((&String::from("Email"), &String::from("TEXT")))
+            );
             Ok(())
         }
     }
